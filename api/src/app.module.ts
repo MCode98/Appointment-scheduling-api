06a269ea336ab12c399dd3011d37fde3816d0c4c
Chiran Jeewantha <chiranjonline@gmail.com>
2023-08-18 21:42:07 +0530

    #2 create database connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      logging: true,
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'root',
+      database: 'bumble_bee',
+      synchronize: true,
+      entities: [User],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
